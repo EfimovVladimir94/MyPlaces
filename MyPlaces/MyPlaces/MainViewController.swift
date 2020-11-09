ad18cf2a9a0b05bf57e9e1ed05838e36ed5d6fde
Efimov <efimov@mail.ru>
2020-11-09 15:03:00 +0300

    added UITableViewController, added pictures for cell

---
+++ MyPlaces/MyPlaces/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  MyPlaces
+//
+//  Created by Пользователь on 09.11.2020.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let objects = ["kitchen", "Black Star Burger", "Oxxxy", "Boocking Machine"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = objects[indexPath.row]
+        cell.imageView?.image = UIImage(named: objects[indexPath.row])
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
